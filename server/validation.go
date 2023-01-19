8baf6cc29c018bb5168325269e94efd7ca001b21
radekska <r.skalbania@piwik.pro>
2023-01-19 21:04:04 +0100

    add editCustomerDetailsRequest

---
+++ server/validation.go
@@ -0,0 +1,7 @@
+package server
+
+type editCustomerDetailsRequest struct {
+	FirstName       string `json:"first_name"       validate:"required"`
+	LastName        string `json:"last_name"        validate:"required"`
+	TelephoneNumber string `json:"telephone_number" validate:"required"`
+}
